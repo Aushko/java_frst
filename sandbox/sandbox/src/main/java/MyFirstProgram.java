b484d96436dea0ee33e506318e3c06ab8cf23c5e
Anna Gau <tmp.a@rambler.ru>
2022-12-24 15:34:46 +0300

    Вычисление расстояния добавлено в класс Point

--- sandbox/sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/sandbox/src/main/java/MyFirstProgram.java
@@ -1,9 +1,26 @@
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("My first program!");
-        System.out.println(distance(new Point(1.0,2.0), new Point(3.0,4.0)));
+        System.out.println("Координаты точек х1у1(1, 2) и х2у2(3, 4)");
+        System.out.println("Расстояние = " + Point.distance(new Point(1.0,2.0), new Point(3.0,4.0)));
+
     }
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt(Math.pow(p2.x- p1.x, 2)+Math.pow(p2.y - p1.y, 2));
+   /* public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
     }
+
+   private static double distance2(Point p1, Point p2) {
+        double x21 = p2.x - p1.x;
+        System.out.println("x2 - x1 = " + x21);
+
+        double y21 = p2.y - p1.y;
+        System.out.println("y2 - y1 = " + y21);
+
+        double x21pow = Math.pow(x21, 2);
+        System.out.println("pow2 x21 = " + x21pow);
+
+        double y21pow = Math.pow(y21, 2);
+        System.out.println("pow y21 = " + y21pow);
+
+        return Math.sqrt(x21pow + y21pow);
+    }*/
 }
--- sandbox/sandbox/src/main/java/Point.java
+++ sandbox/sandbox/src/main/java/Point.java
@@ -8,4 +8,22 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+    public static double distance(Point p1, Point p2){
+        double x21 = p2.x - p1.x;
+        System.out.println("x2 - x1 = " + x21);
+
+        double y21 = p2.y - p1.y;
+        System.out.println("y2 - y1 = " + y21);
+
+        double x21pow = Math.pow(x21, 2);
+        System.out.println("(x2 - x1)^2 = " + x21pow);
+
+        double y21pow = Math.pow(y21, 2);
+        System.out.println("(y2 - y1)^2 = " + y21pow);
+
+        double sPow = x21pow+y21pow;
+        System.out.println("(x2 - x1)^2 + (y2 - y1)^2 = " + sPow);
+
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2)+Math.pow(p2.y - p1.y, 2));
+    }
 }
