2c4bc186ad04c06d59c2776ce87c90f27c8e83d4
Anna Gau <tmp.a@rambler.ru>
2022-12-24 14:03:45 +0300

    создание функции

--- sandbox/sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,9 @@
 public class MyFirstProgram {
-
     public static void main(String[] args) {
         System.out.println("My first program!");
+        System.out.println(distance(new Point(1.0,2.0), new Point(3.0,4.0)));
+    }
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p2.x- p1.x, 2)+Math.pow(p2.y - p1.y, 2));
     }
 }
